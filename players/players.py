162cac77ec6ad84714d560e55ec4a0f19eccf7f6
WW9kYQ <maxence.huet2003@gmail.com>
2023-04-03 12:27:19 +0200

    update api interface and players.py
    Complete createPlayer, not tested

--- players/players.py
+++ players/players.py
@@ -1,3 +1,6 @@
+import re
+import uuid
+
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -5,7 +8,7 @@ app = Flask(__name__)
 player1 = {
     "id_player": '34afa4d7-2bcb-4290-9906-56ea3f0553eb',
     "id_user": 'f97c4650-4795-4232-9a62-85eb97be71aa',
-    "list_id_chall_reussi": [
+    "list_id_chall_success": [
         "Fl@g_!"
     ],
     "list_id_chall_try": [
@@ -18,7 +21,7 @@ player1 = {
 player2 = {
     "id_player": 'e795512c-db6c-4bbe-8a4c-544107d24f0f',
     "id_user": '71335eb2-4360-4515-bd7d-894da5e24e19',
-    "list_id_chall_reussi": [
+    "list_id_chall_success": [
         "Fl@g_!"
     ],
     "list_id_chall_try": [
@@ -28,6 +31,7 @@ player2 = {
     "username": 'titi'
 }
 
+players = [player1,player2]
 
 @app.route('/')
 def hello_world():  # put application's code here
@@ -36,25 +40,39 @@ def hello_world():  # put application's code here
 
 @app.route('/players', methods=['GET'])
 def getPlayers():
-    tab_players = [player1, player2]
 
-    return jsonify(tab_players)
+    return jsonify(players)
     # TODO getPlayers, get data from DB
 
 
 @app.route('/players/create', methods=['POST'])
 def createPlayers():
-    createdPlayer = {}
+    createdPlayer = {
+        "id_player": str(uuid.uuid4()),
+        "id_user": "",
+        "list_id_chall_success": [],
+        "list_id_chall_try": [],
+        "id_game": "",
+        "username": ""
+    }
     valid_key = ["id_user", "username", "id_game"]
     rq = request.get_json()
-    tab_key=[]
+    provided_key = []
+    regex_uuid = re.compile(r"^[0-9a-zA-Z]{8}(-[0-9a-zA-Z]{4}){3}-[0-9a-zA-Z]{12}$")
+    regex_username = re.compile(r"^[\w\d\-]{3,32}$")
     for key in rq:
         if key in valid_key:
-            tab_key.append(key)
+            provided_key.append(key)
         else:
-            return "Bad informations were given", 400
-    # print(rq)
-    return jsonify(tab_key)
+            return "Bad informations were given", 405
+        if provided_key == valid_key:
+            if regex_uuid.match(rq['id_user']) and regex_uuid.match(rq['id_game']) and regex_username.match(rq['username']):
+                createdPlayer['id_user'] = rq['id_user']
+                createdPlayer['id_game'] = rq['id_game']
+                createdPlayer['username'] = rq['username']
+                players.append(createdPlayer)
+                
+    return createdPlayer
     # TODO createPlayers, update the database
 
 
