5209ee35941df3bf844cad1dc4b80013117a5129
perrze <partzival@partzival.net>
2023-04-08 22:15:58 +0000

    Adding snippets + functions

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -5,4 +5,11 @@
 /* ----------------------------- Global Var sets ---------------------------- */
 let BASE_URL="http://api.ctf401.bb0.fr"
 
-
+$( document ).ready(function(){
+    if(Cookies.get("jwt")==undefined){
+        $("#divLoginModal").load('/snippets/users/loginModal.html');
+        $("#userManagement").load('/snippets/users/loginButton.html');
+    }else{
+        $("#userManagement").load('/snippets/users/loggedIn.html'); 
+    }
+});
--- frontend/js/users.js
+++ frontend/js/users.js
@@ -7,16 +7,18 @@ function login(){
         type: 'get',
         statusCode: {
             200: function(response) {
-                // console.log("200")
+                // console.log("200");
                 // console.log(response);
-                var token = response.token
-                // console.log(token)
-                Cookies.set('jwt',token,{ expires :1,sameSite:'Lax' })
-                getUser()
-                $('#loginModal').modal('hide')
+                var token = response.token;
+                // console.log(token);
+                Cookies.set('jwt',token,{ expires :1,sameSite:'Lax' });
+                // await new Promise(r => setTimeout(r, 2000));
+                getUser();
+                $('#loginModal').modal('hide');
+                
             },
             401: function(xhr) {
-                console.log("401")
+                console.log("401");
                 console.log(xhr.responseText);
                 $('#tooltipLoginModal').removeAttr('hidden');
             }
@@ -26,11 +28,12 @@ function login(){
 }
 
 function logout(){
-    Cookies.remove('jwt')
-    Cookies.remove('email')
-    Cookies.remove('userid')
-    Cookies.remove('description')
-    Cookies.get('jwt')
+    Cookies.remove('jwt');
+    Cookies.remove('email');
+    Cookies.remove('userid');
+    Cookies.remove('description');
+    Cookies.get('jwt');
+    location.reload();
 }
 
 function getUser(){
@@ -43,12 +46,14 @@ function getUser(){
             200: function(response) {
                 // console.log("200")
                 console.log(response);
-                Cookies.set('email',response.email,{ expires :1,sameSite:'Lax' })
-                Cookies.set('userid',response.id_user,{ expires :1,sameSite:'Lax' })
-                Cookies.set('description',response.description,{ expires :1,sameSite:'Lax' })
+                Cookies.set('email',response.email,{ expires :1,sameSite:'Lax' });
+                Cookies.set('userid',response.id_user,{ expires :1,sameSite:'Lax' });
+                Cookies.set('description',response.description,{ expires :1,sameSite:'Lax' });
+                Cookies.set('roles',String(response.roles),{ expires :1,sameSite:'Lax' });
+                location.reload();
             },
             401: function(xhr) {
-                console.log("401")
+                console.log("401");
                 console.log(xhr.responseText);
                 
             }
