e97c305311fbeaeb5bc891c67192ed66e451fc38
louisperard <129521063+louisperard@users.noreply.github.com>
2023-04-13 13:36:49 +0200

    Update team_programme_statique
    rename the files programme_teams.py in team_programme_statique.py and update this

--- teams/team_programme_statique.py
+++ teams/team_programme_statique.py
@@ -6,24 +6,28 @@ from types import SimpleNamespace as Namespace
 import requests
 
 dico = [
-	{"description": "Apprenti",
-	"id_game": "11",
-	"id_team": "1",
-	"members": ["1", "2", "3", "4", "5", "6", "7"],
-	"name": "Apprenti",},
-	{"description": "champion",
-	"id_game": "22",
-	"id_team": "2",
-	"members": ["1", "2", "3", "4", "5", "6", "7"],
-	"name": "champion",},
-	{"description": "alza",
-	"id_game": "33",
-	"id_team": "3",
-	"members": ["1", "2", "3", "4", "5", "6", "7"],
-	"name": "alza",}]
-
-print(dico["description"])
-#(([0-9a-z]{8})-([0-9a-z]{4})-([0-9a-z]{4})-([0-9a-z]{4})-([0-9a-z]{12}))
+	{
+		"description": "Apprenti",
+		"id_game": "11",
+		"id_team": "1",
+		"members": ["1", "2", "3", "4", "5", "6", "7"],
+		"name": "Apprenti",
+	},
+		   {
+		"description": "champion",
+		"id_game": "22",
+		"id_team": "2",
+		"members": ["1", "2", "3", "4", "5", "6", "7"],
+		"name": "champion",
+	},
+	{
+		"description": "alza",
+		"id_game": "33",
+		"id_team": "3",
+		"members": ["1", "2", "3", "4", "5", "6", "7"],
+		"name": "alza",
+	}
+]
 
 
 app = Flask(__name__)
@@ -44,36 +48,47 @@ def getAllTeams():
 @app.route("/teams", methods=["POST"])
 def postTeam():
 	if request.method == 'POST':
-		rep = request.get_json()
+		rep = request.json
 		id_team = uuid.uuid4()
 		description = rep['description']
-		member = rep['member']
-		name = rep['name']
+		member = rep["faire appel à l'API"]
+		name = rep["name"]
 		id_game = "faire appel à l'API"
-		team = {"id_team": id_team, "description": description, "member": member, "nama": name, "id_game": id_game}
+		team = {"id_team": id_team, "description": description, "member": member, "name": name, "id_game": id_game}
 		dico.append(team)
-		return jsonify({'200':"Challenge created"})
-
+		return jsonify({'200':"team created"})
 
 
 @app.route("/teams/<id_team>", methods=['GET'])
 def getTeam(id_team):
 	if request.method == 'GET':
-		for team in dico :
-			if str(id_team) == str(team):
-				return jsonify({'200':"team found",'info':dico[id_team]})
+		for team in dico:
+			if id_team == team["id_team"]:
+				return jsonify({'200':"team found",'info':team["id_team"]})
 		return jsonify({'404':"ID not found"})
 
 
 @app.route("/teams/<id_team>", methods=["DELETE"])
 def deleteTeam(id_team):
 	if request.method == 'DELETE':
-		return "deleteTeam"
+		for team in dico :
+			if (team['id_challenge'] == id_team) :
+				dico.remove(team)
+				return jsonify({'200':"team deleted"})
+		return jsonify({'404':"ID not found"})
+
 
 @app.route("/teams/<id_team>", methods=["PATCH"])
 def patchTeam(id_team):
 	if request.method == 'PATCH':
-		return "patchTeam"
+		for team in dico :
+			if (team['id_team'] == id_team) :
+				response = request.get_json()
+				for key, value in response.items():
+					if key in team:
+						team[key] = value
+						return jsonify({'200':"team Modified"})
+		return jsonify({'404':"ID not found"})
 
 
 @app.route("/teams/name", methods=['GET'])
@@ -81,17 +96,18 @@ def getAllNameTeam():
 	if request.method == 'GET':
 		liste_name=[]
 		for i in dico:
-			l =  dico["name"]
+			l = i["name"]
 			liste_name.append(l)
+		return liste_name
 		return jsonify({'200':"name of all team has found","name" : liste_name})
 
 
 @app.route("/teams/name/<id_team>", methods=['GET'])
 def getNameTeam(id_team):
 	if request.method == 'GET':
-		for team in dico :
-			if str(id_team) == str(team):
-				return jsonify({'200':"name of this team has found",'info':dico[id_team]["name"]})
+		for team in dico:
+			if id_team == team["id_team"]:
+				return jsonify({'200':"name of this team has found",'info':team["name"]})
 		return jsonify({'404':"ID not found"})
 
 
@@ -99,23 +115,27 @@ def getNameTeam(id_team):
 def getMembersTeam(id_team):
 	if request.method == 'GET':
 		for team in dico :
-			if str(id_team) == str(team):
-				return jsonify({'200':"member for this team found",'info':dico[id_team]["members"]})
+			if id_team == team["id_team"]:
+				return jsonify({'200':"member for this team found",'info':team["members"]})
 		return jsonify({'404':"ID not found"})
 
 
 @app.route("/teams/member/<id_team>", methods=["DELETE"])
 def deleteMemberTeam(id_team):
 	if request.method == 'DELETE':
-		return "deleteMemberTeam"
+		for team in dico :
+			if (team['id_challenge'] == id_team) :
+				dico.remove(team)
+				return jsonify({'200':"member of team deleted"})
+		return jsonify({'404':"ID not found"})
 
 
 @app.route("/teams/description/<id_team>", methods=['GET'])
 def getDescriptionTeam(id_team):
 	if request.method == 'GET':
 		for team in dico :
-			if str(id_team) == str(team):
-				return jsonify({'200':"description for this team has found",'info':dico[id_team]["description"]})
+			if id_team == team["id_team"]:
+				return jsonify({'200':"description for this team has found",'info':team["description"]})
 		return jsonify({'404':"ID not found"})
 
 
@@ -123,10 +143,10 @@ def getDescriptionTeam(id_team):
 def getWhenGame(id_team):
 	if request.method == 'GET':
 		for team in dico :
-			if str(id_team) == str(team):
-				return jsonify({'200':"team found",'info':dico[id_team]["id_game"]})
+			if id_team == team["id_team"]:
+				return jsonify({'200':"team found",'info':team["id_game"]})
 		return jsonify({'404':"ID not found"})
 
 
 if __name__ == '__main__':
-	app.run(port=8080, debug="on")
+	app.run(port=5000, debug="on")
